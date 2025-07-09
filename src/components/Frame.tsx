2fdbfe476463f51f3f27bcd43a1cab58af8719f4
DBRH-52 <dbrhx52@gmail.com>
2025-07-09 15:40:25 +0200

    frame component

---
+++ src/components/Frame.tsx
@@ -0,0 +1,11 @@
+export default function Frame({
+    selectedType, selectedItem, selectedLanguage
+}) {
+    return (
+        <div className="frames-container">
+            <div className="visualization-frame">...</div>
+            <div className="code-frame">...</div>
+            <div className="explaination-frame">...</div>
+        </div>
+    );
+}
