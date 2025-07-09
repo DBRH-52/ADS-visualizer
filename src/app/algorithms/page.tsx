7cd229f9094344e5b147b1908c58513bffdaef21
DBRH-52 <dbrhx52@gmail.com>
2025-07-09 15:22:59 +0200

    algorithms: dropdown buttons

--- src/app/algorithms/page.tsx
+++ src/app/algorithms/page.tsx
@@ -1,9 +1,71 @@
+"use client";
+
 import './algorithms.css';
 
+import { useState } from 'react';
+
 export default function Algorithms() {
+
+    const [selectedAlgorithmType, setSelectedAlgorithmType] = useState('');
+    const [selectedAlgorithm, setSelectedAlgorithm] = useState('');
+    const [selectedLanguage, setSelectedLanguage] = useState('');
+
+    const algorithms = {
+        sorting: ['bubble-sort', 'quick-sort'],
+        searching: ['binary-search', 'linear-search']
+    };
+
+    const handleAlgorithmTypeChange = (e) => { // e - event object 
+        setSelectedAlgorithmType(e.target.value);
+        setSelectedAlgorithm('');
+    };
+
+    const handleAlgorithmChange = (e) => {
+        setSelectedAlgorithm(e.target.value);
+    };
+
+    const handleLanguageChange = (e) => {
+        setSelectedLanguage(e.target.value);
+    };
+
     return (
       <div className="algorithms-container">
         <h1 className="algorithms-title">Algorithms</h1>
+
+        <div className="dropdowns-container">
+
+            <select 
+                className="dropdown"
+                value={selectedAlgorithmType}
+                onChange={handleAlgorithmTypeChange}
+                >
+                <option value="">algorithm type</option>
+                <option value="sorting">sorting</option>
+                <option value="searching">searching</option>
+            </select>
+
+            <select 
+                className="dropdown"
+                value={selectedAlgorithm}
+                onChange={handleAlgorithmChange}
+            >
+                <option value="">algorithm</option>
+                <option value="bubble-sort">bubble sort</option>
+                <option value="quick-sort">quick sort</option>
+            </select>
+        
+            <select 
+                className="dropdown"
+                value={selectedLanguage}
+                onChange={handleLanguageChange}
+            >
+                <option value="">language</option>
+                <option value="python">python</option>
+                <option value="javascript">javascript</option>
+            </select>
+
+        </div>
+
       </div>
     );
   }
