7d0a95735e585c234f463aab31a13bf454a1db2b
DBRH-52 <dbrhx52@gmail.com>
2025-07-14 00:14:24 +0200

    algorithms code print in frame

--- src/components/Frame/FrameView.tsx
+++ src/components/Frame/FrameView.tsx
@@ -1,5 +1,8 @@
 import './Frame.css';
 
+import {getAlgorithmCode} from '../../../content/algorithms/index';
+//import {getDataStructureCode} from '../../../content/data-structures/index';
+
 type FrameProps = {
     selectedType: string;
     selectedItem: string;
@@ -11,10 +14,33 @@ export default function Frame({
     selectedItem, 
     selectedLanguage
 }: FrameProps) {
+
+    let code = '';
+    if (selectedItem && selectedLanguage){
+        code = getAlgorithmCode(selectedItem, selectedLanguage);
+    }
+    // else if (selectedType === 'data-structures') {
+    //     code = getDataStructureCode(selectedItem, selectedLanguage);
+    // }
+
+    console.log('selectedType:', selectedType);
+    console.log('selectedItem:', selectedItem);
+    console.log('selectedLanguage:', selectedLanguage);
+    console.log('code:', code);
+    console.log('--------------------------------');
+
     return (
         <div className="frames-container">
             <div className="visualization-frame">visualization</div>
-            <div className="code-frame">code</div>
+
+            <div className="code-frame">
+                <pre>
+                    <code>
+                        {code}
+                    </code>
+                </pre>
+            </div>
+
             <div className="explanation-frame">explanation</div>
         </div>
     );
